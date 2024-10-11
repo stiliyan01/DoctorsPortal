7b2ea8e80916dfb4e327015d0856bfda88dabfb0
stiliyan <stiliyan_2001@abv.bg>
2024-10-11 10:21:49 +0300

    Creating the layout

---
+++ views/partials/dashboardPanel.php
@@ -0,0 +1,19 @@
+
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="index.html">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    tuk moje da slojish username
+                </div>
+        </nav>
+    </div>
+    <div id="layoutSidenav_content">
