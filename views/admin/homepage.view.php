ba0fcac409ca0ecf88d6d305aaa10e3ad073f4f6
stiliyan <stiliyan_2001@abv.bg>
2024-10-13 00:14:32 +0300

    Refactor homepage view to remove breadcrumb and add user profile form

--- views/admin/homepage.view.php
+++ views/admin/homepage.view.php
@@ -8,55 +8,43 @@
 
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Home</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Profile</li>
-        </ol>
-        <div class="row">
+        <div class=''>
+            <h1 class="mt-4">Home</h1>
 
         </div>
 
-
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-table me-1"></i>
-                Profile
+        <form method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label">Име</label>
+                <input type="text" name="name" class="form-control" id="name" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Имейл</label>
+                <input type="email" name="email" class="form-control" id="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Парола</label>
+                <input type="password" name="password" class="form-control" id="password" required>
+            </div>
+            <div class="mb-3">
+                <label for="options" class="form-label">Специалност</label>
+                <select class="form-select" name="options" id="options" required>
+                    <option selected disabled>Изберете специалност</option>
+                    <option value="option1">Опция 1</option>
+                    <option value="option2">Опция 2</option>
+                    <option value="option3">Опция 3</option>
+                    <!-- Можеш да добавиш още опции тук -->
+                </select>
             </div>
 
-        </div>
+            <button type="submit" class="btn btn-primary">Запази</button>
+        </form>
+
     </div>
 </main>
 
 
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        height: 650,
-        events: [{
-                title: 'event1',
-                start: '2024-10-11'
-            },
-            {
-                title: 'event2',
-                start: '2010-01-05',
-                end: '2010-01-07'
-            },
-            {
-                title: 'event3',
-                start: '2024-10-11T11:30:00',
-                end: '2024-10-11T12:30:00',
-                allDay: false // will make the time show
-            }
 
-        ]
-
-    });
-    calendar.render();
-});
-</script>
 <?php
-require('views/partials/footer.php');
-
+    require('views/partials/footer.php');
 ?>
