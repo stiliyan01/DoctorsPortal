9bbaffd2791e2eff9974b200fe1d79d13c5667c7
stiliyan <stiliyan_2001@abv.bg>
2024-10-15 20:10:53 +0300

    Update admin dashboard panel links and headings to Bulgarian language

--- views/partials/admin/dashboardPanel.php
+++ views/partials/admin/dashboardPanel.php
@@ -3,24 +3,24 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark bg-primary" id="sidenavAccordion">
             <div class="sb-sidenav-menu d-flex-column">
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Dashboard</div>
+                    <div class="sb-sidenav-menu-heading text-white">Контролен панел</div>
 
                     <!-- Calendar link -->
                     <a class="nav-link <?=  urlIs('/admin') ? 'active' : ''; ?>" href="/admin">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-user-doctor"></i></i></div>
-                        Home
+                        Профил
                     </a>
 
                     <!-- Calendar link -->
                     <a class="nav-link <?=  urlIs('/admin/calendar') ? 'active' : ''; ?>" href="/admin/calendar">
                         <div class="sb-nav-link-icon"><i class="fa-regular fa-calendar"></i></div>
-                        Calendar
+                        Календар
                     </a>
 
                     <!-- Patients link -->
                     <a class="nav-link <?=  urlIs('/admin/patients') ? 'active' : ''; ?>" href="/admin/patients">
                         <div class="sb-nav-link-icon"><i class="fa-solid fa-hospital-user"></i></div>
-                        Patients
+                        Пациенти
                     </a>
                 </div>
                 <div class="sb-sidenav-footer align-items-end justify-content-end bg-primary">
