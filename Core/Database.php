495125aa2f09ba173abfa8940e925f89c5c892f3
stiliyan <stiliyan_2001@abv.bg>
2024-11-04 16:31:13 +0200

    Refactor Database and Model classes to implement Singleton pattern for database connection

--- Core/Database.php
+++ Core/Database.php
@@ -1,24 +1,32 @@
 <?php
 
-$config = require 'config.php';
-class Database{
-    public  $connection;
+class Database {
+    private static $instance = null;
+    public $connection;
+    public $statement;
 
-    public  $statement;
-    public function __construct($config){
-        $this->connection = new PDO(getDsn($config), $config["username"], $config['password'], [
-            PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_ASSOC,
-        ]);
+    private function __construct($config) {
+        $this->connection = new PDO(
+            getDsn($config),
+            $config["username"],
+            $config['password'],
+            [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );
     }
 
-    public function query($sql, $params = []){
+    // Singleton method to get the single instance of Database
+    public static function getInstance($config) {
+        if (self::$instance === null) {
+            self::$instance = new Database($config['database']);
+        }
+        return self::$instance;
+    }
+
+    public function query($sql, $params = []) {
         $this->statement = $this->connection->prepare($sql);
         $this->statement->execute($params);
-
         return $this;
     }
-
-  
 }
-
-$db = new Database($config['database']);
--- Core/Model.php
+++ Core/Model.php
@@ -1,57 +1,58 @@
 <?php
 
-class Model extends Database
-{
+class Model {
     protected $table;
     protected $primaryKey = 'id';
+    protected $db;
 
-    public function query($sql, $params = [])
-    {
-        $this->statement = $this->connection->prepare($sql);
-        $this->statement->execute($params);
+    public function __construct() {
+        global $config; 
+        $this->db = Database::getInstance($config); 
+    }
+
+    public function query($sql, $params = []) {
+        $this->db->query($sql, $params);
         return $this;
     }
 
-    public function all()
-    {
+    public function all() {
         $sql = "SELECT * FROM {$this->table}";
         $this->query($sql);
-        return $this->statement->fetchAll();
+        return $this->db->statement->fetchAll();
     }
 
-    public function find($id)
-    {
+    public function find($id) {
         $sql = "SELECT * FROM {$this->table} WHERE {$this->primaryKey} = :id";
         $this->query($sql, ['id' => $id]);
-        return $this->statement->fetch();
+        return $this->db->statement->fetch();
     }
 
     public function delete($id)
-    {
-        $sql = "DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id";
-        $this->query($sql, ['id' => $id]);
-        return $this->statement->rowCount();
-    }
-
-    public function create(array $data)
-    {
-        $columns = implode(', ', array_keys($data));
-        $placeholders = ':' . implode(', :', array_keys($data));
-        $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$placeholders})";
-        $this->query($sql, $data);
-        return $this->statement->rowCount();
-    }
-
-    public function update($id, array $data)
-    {
-        $fields = '';
-        foreach ($data as $key => $value) {
-            $fields .= "{$key} = :{$key}, ";
+        {
+            $sql = "DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id";
+            $this->query($sql, ['id' => $id]);
+            return $this->db->statement->rowCount();
         }
-        $fields = rtrim($fields, ', ');
-        $data[$this->primaryKey] = $id;
-        $sql = "UPDATE {$this->table} SET {$fields} WHERE {$this->primaryKey} = :{$this->primaryKey}";
-        $this->query($sql, $data);
-        return $this->statement->rowCount();
-    }
+    
+        public function create(array $data)
+        {
+            $columns = implode(', ', array_keys($data));
+            $placeholders = ':' . implode(', :', array_keys($data));
+            $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$placeholders})";
+            $this->query($sql, $data);
+            return $this->db->statement->rowCount();
+        }
+    
+        public function update($id, array $data)
+        {
+            $fields = '';
+            foreach ($data as $key => $value) {
+                $fields .= "{$key} = :{$key}, ";
+            }
+            $fields = rtrim($fields, ', ');
+            $data[$this->primaryKey] = $id;
+            $sql = "UPDATE {$this->table} SET {$fields} WHERE {$this->primaryKey} = :{$this->primaryKey}";
+            $this->query($sql, $data);
+            return $this->db->statement->rowCount();
+        }
 }
