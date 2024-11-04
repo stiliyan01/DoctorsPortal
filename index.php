5bef096f0d4262244a34346b34115ee09c6409f0
stiliyan <stiliyan_2001@abv.bg>
2024-11-04 16:32:12 +0200

    Refactor index.php to improve routing and configuration loading

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
 session_start();
+
+$config = require 'config.php';
+  
 require 'Core/functions.php';
 require 'Core/Database.php';
-require 'Core/router.php';
+require 'Core/Router.php';
 
+$router = new Router();
+$routes = require 'routes.php';
 
-	
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-
-
-
-	
+$router->routeTo($uri, $method);
