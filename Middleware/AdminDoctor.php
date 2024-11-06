c968fb0d78b4d384dfdd3193d46cbe46cb73b954
stiliyan <stiliyan_2001@abv.bg>
2024-11-06 14:20:58 +0200

    Add session check in AdminDoctor middleware to redirect unauthenticated users

--- Middleware/AdminDoctor.php
+++ Middleware/AdminDoctor.php
@@ -5,6 +5,11 @@ require_once './Core/Middleware.php';
 class AdminDoctor extends Middleware
 {
     public function handle(){
+        if(!isset($_SESSION['user'])){
+            header('location: /login');
+            exit();
+        }
+        
         if ($_SESSION['user']['is_doctor'] === false) {
             header('location: /');
             exit();
