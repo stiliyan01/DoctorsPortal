a2578bba6cd1b4d34a77cc1f68e2647a45142606
stiliyan <stiliyan_2001@abv.bg>
2024-11-06 13:43:44 +0200

    Replace UserMiddleware with User model in login controller

--- Controllers/login.php
+++ Controllers/login.php
@@ -3,7 +3,7 @@
 require('Models/User.php');
 require('Models/Doctor.php');
 
-$userModel= new UserMiddleware();
+$userModel= new User();
 $doctorModel = new Doctor();
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
