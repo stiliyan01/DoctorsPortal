72345bccdc4916ed6388500713064854a71486a0
stiliyan <stiliyan_2001@abv.bg>
2024-11-06 16:42:59 +0200

    Enhance navbar branding: update logo text to bold and uppercase

--- views/partials/admin/nav.view.php
+++ views/partials/admin/nav.view.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="/admin">Doc Port</a>
+    <a class="navbar-brand ps-3 font-weight-bold" href="/admin"><strong>DOC PORT</strong></a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 text-white" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
