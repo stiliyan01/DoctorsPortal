53b085e77f6bf6b948ae532812ecbe826dc2703c
stiliyan <stiliyan_2001@abv.bg>
2024-11-06 14:58:19 +0200

    Update navbar brand link and enhance user profile navigation logic

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="#page-top">DOC PORT</a>
+        <a class="navbar-brand" href="/">DOC PORT</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
@@ -16,11 +16,14 @@
                 </li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
                         href='/register'>Регистрирай се</a></li>
-                <?php else : ?>
+                <?php endif; ?>
+
+                <?php if (isset($_SESSION['user']) && (current_uri() !== strtoupper('/user-profile') )) : ?>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
                         href='user-profile'>Профил</a></li>
                 <?php endif;  ?>
-                <?php if (isset($_SESSION['user'])) : ?>
+
+                <?php if (isset($_SESSION['user']) && urlIs('/user-profile')) : ?>
                 <li class="nav-item mx-0 mx-lg-1">
                     <form action="/logout" method="POST" style="display: inline;">
                         <input type="hidden" name="_method" value="DELETE">
