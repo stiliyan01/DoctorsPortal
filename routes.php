90818464be355f892137744672da67dfce19fde4
stiliyan <stiliyan_2001@abv.bg>
2024-11-07 15:59:42 +0200

    Refactor routing: uncomment and organize admin, user, and authentication routes

--- routes.php
+++ routes.php
@@ -1,38 +1,38 @@
 <?php 
-$router->get('/admin', 'Controllers/admin/Test.php', 'index');
-// //admin-doctors
-// $router->get('/admin', 'Controllers/admin/homepage.php')->only('admin_doctor');
-// $router->get('/admin/calendar', 'Controllers/admin/calendar.php')->only('admin_doctor');
-// $router->get('/admin/patients', 'Controllers/admin/patients/patients.php')->only('admin_doctor');
-// $router->get('/admin/patient-details', 'Controllers/admin/patients/patient-details.php')->only('admin_doctor');
-// $router->get('/admin/donors', 'Controllers/admin/donors.php')->only('admin_doctor');
 
-// $router->put('/admin/update-patient-details', 'Controllers/admin/patients/update-patient-details.php')->only('admin_doctor');
+//admin-doctors
+$router->get('/admin', 'Controllers/admin/homepage.php')->only('admin_doctor');
+$router->get('/admin/calendar', 'Controllers/admin/calendar.php')->only('admin_doctor');
+$router->get('/admin/patients', 'Controllers/admin/patients/patients.php')->only('admin_doctor');
+$router->get('/admin/patient-details', 'Controllers/admin/patients/patient-details.php')->only('admin_doctor');
+$router->get('/admin/donors', 'Controllers/admin/donors.php')->only('admin_doctor');
 
-
-// $router->put('/admin/update-doctor', 'Controllers/admin/update-doctor.php')->only('admin_doctor');
+$router->put('/admin/update-patient-details', 'Controllers/admin/patients/update-patient-details.php')->only('admin_doctor');
 
 
-// //admin-user
-// $router->get('/user-profile', 'Controllers/user/user-profile.php')->only('user');
-// $router->put('/update-user-profile', 'Controllers/user/update-user-profile.php')->only('user');
-
-// //login
-// $router->get('/login', 'Controllers/login.php')->only('guest');
-// $router->post('/login-account', 'Controllers/login.php');
+$router->put('/admin/update-doctor', 'Controllers/admin/update-doctor.php')->only('admin_doctor');
 
 
-// //register
-// $router->get('/register', 'Controllers/register.php')->only('guest');
-// $router->post('/register-account', 'Controllers/register.php');
+//admin-user
+$router->get('/user-profile', 'Controllers/user/user-profile.php')->only('user');
+$router->put('/update-user-profile', 'Controllers/user/update-user-profile.php')->only('user');
+
+//login
+$router->get('/login', 'Controllers/login.php')->only('guest');
+$router->post('/login-account', 'Controllers/login.php');
 
 
-// //logout
-// $router->delete('/logout', 'Controllers/logout.php');
+//register
+$router->get('/register', 'Controllers/register.php')->only('guest');
+$router->post('/register-account', 'Controllers/register.php');
 
 
-// //user
-// $router->get('/', 'Controllers/homepage.php');
-// $router->get('/doctors', 'Controllers/doctor-results.php');
-// $router->get('/save-time', 'Controllers/save-time.php');
-// $router->post('/save-time-to-db', 'Controllers/save-time-to-db.php')->only('user');
+//logout
+$router->delete('/logout', 'Controllers/logout.php');
+
+
+//user
+$router->get('/', 'Controllers/homepage.php');
+$router->get('/doctors', 'Controllers/doctor-results.php');
+$router->get('/save-time', 'Controllers/save-time.php');
+$router->post('/save-time-to-db', 'Controllers/save-time-to-db.php')->only('user');
