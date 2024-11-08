38e9e094e8025080e6c70c8f9298f4cdeedd2e59
stiliyan <stiliyan_2001@abv.bg>
2024-11-08 07:55:06 +0200

    Remove unnecessary blank line in login controller

--- Controllers/login.php
+++ Controllers/login.php
@@ -5,5 +5,4 @@ $parsed = parse_url($referer);
 $location = ltrim($parsed['path'] ?? '', '/') . (isset($parsed['query']) ? '?' . $parsed['query'] : '');
 $_SESSION['last_location_before_login'] = $location;  
 
-
 view('login');
