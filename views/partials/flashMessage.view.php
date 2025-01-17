3bbf5e5619e285ee4c2c359cd47a7d5077848c85
stiliyan <stiliyan_2001@abv.bg>
2025-01-17 20:37:50 +0200

    Add flash message component for displaying success alerts

---
+++ views/partials/flashMessage.view.php
@@ -0,0 +1,6 @@
+<div class="position-fixed top-0 end-0 p-3" style="z-index: 1050;">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <?php echo $errors ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</div>
